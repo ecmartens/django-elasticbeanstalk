76a730c117b2a2018df61138a75c517878319f4a
Eric Martens <ericmartens01@gmail.com>
2020-06-04 13:20:04 -0400

    Add basic functionality with updates to urls, models, views, admin

--- artist/admin.py
+++ artist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Genre, Group, Instrument, Musician
+
+admin.site.register(Genre)
+admin.site.register(Group)
+admin.site.register(Instrument)
+admin.site.register(Musician)
--- artist/models.py
+++ artist/models.py
@@ -1,4 +1,7 @@
+from datetime import date
+
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Instrument(models.Model):
@@ -10,12 +13,19 @@ class Instrument(models.Model):
 
 class Musician(models.Model):
     '''Model representing a musician.'''
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    group = models.ForeignKey('Group', on_delete=models.SET_NULL, null=True, blank=True, related_name='+')
     instrument = models.ManyToManyField(Instrument, help_text="Select instrument(s) for this musician")
+    photo = models.CharField(max_length=200, default="undefined")
+    hometown = models.CharField(max_length=25, default="undefined")
 
     def __str__(self):
-        return self.first_name, self.last_name
+        return self.first_name + ' ' + self.last_name
+
+    # def get_absolute_url(self):
+    #     '''returns the url for a specific musician.'''
+    #     return reverse('musician-detail', args=[str(self.id)])
 
 class Genre(models.Model):
     """Model representing a book genre (e.g. Science Fiction, Non Fiction)."""
@@ -28,5 +38,10 @@ class Group(models.Model):
     '''Model representing a music group'''
     name = models.CharField(max_length=25, help_text="Enter the group's name.")
     genre = models.ManyToManyField('Genre', help_text="Enter a genre.")
+    origin = models.CharField(max_length=25, default="")
+    date_formed = models.DateField(default=date.today)
+    photo = models.CharField(max_length=200, default="")
+    members = models.ManyToManyField('Musician', help_text="enter musicians.", related_name='+')
 
-
+    def __str__(self):
+        return self.name
