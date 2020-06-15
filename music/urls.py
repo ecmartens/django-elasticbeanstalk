87628a1be1fcb020e5a8ea971e7ad1e8d00c9e58
Eric Martens <eric.martens@protonmail.com>
2020-06-15 14:28:25 -0400

    Add MEDIA_URL, MEDIA_ROOT to settings and urls, update Bootstrap CDN in templates

--- music/urls.py
+++ music/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -29,3 +31,5 @@ urlpatterns += [
 urlpatterns += [
     path('', RedirectView.as_view(url='/home/', permanent=True)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
